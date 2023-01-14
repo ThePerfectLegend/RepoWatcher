813f52551999c495d02829b9b0e7722182b40abd
Nizami <nizami@about-me.app>
2023-01-14 11:54:16 +0500

    Initial Commit

---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Nizami Tagiyev on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
