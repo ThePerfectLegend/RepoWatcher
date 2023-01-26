5c79edf9e5158970765a0fd902e359e284a1e529
Nizami <nizami@about-me.app>
2023-01-26 17:11:48 +0500

    Creating List of Repos in App

---
+++ RepoWatcher/Extensions/UserDefaults.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaults.swift
+//  RepoWatcher
+//
+//  Created by Nizami Tagiyev on 26.01.2023.
+//
+
+import Foundation
+
+extension UserDefaults {
+    static var shared: UserDefaults {
+        UserDefaults(suiteName: "group.NizamiDev.RepoWatcher")!
+    }
+    
+    static let repoKey = "repos"
+}
